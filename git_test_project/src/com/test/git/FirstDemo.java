2f7a244550bf041f46715a11e966317276432261
1234 <1234@qq.com>
2019-08-31 15:43:14 +0800

    把本地工程添加到git

---
+++ git_test_project/src/com/test/git/FirstDemo.java
@@ -0,0 +1,13 @@
+package com.test.git;
+
+/**
+ * @author chen
+ * @create 2019-08-31 9:56
+ */
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("===============");
+        System.out.println("hello world");
+    }
+}
